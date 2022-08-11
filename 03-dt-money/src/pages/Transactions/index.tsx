01f36daf2bc24f7fee9a3854f7e526c1f3fc5433
Murillo Mesquita <mu.pracucio@gmail.com>
2022-08-11 18:36:09 -0300

    Desenvolvimento da aplicacao DT Money

---
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -0,0 +1,46 @@
+import { useContextSelector } from 'use-context-selector'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionsTable,
+} from './styles'
+
+export function Transactions() {
+  const transactions = useContextSelector(TransactionContext, (context) => {
+    return context.transactions
+  })
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
+    </div>
+  )
+}
